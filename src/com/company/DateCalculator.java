f275baa42f8c4c1b5e9231fe494e2edd848afae5
Marco <marcolam053@gmail.com>
2019-03-31 00:57:23 +1100

    Unit Test Commit

--- src/com/company/DateCalculator.java
+++ src/com/company/DateCalculator.java
@@ -14,7 +14,7 @@ public class DateCalculator {
     private  static String endDate = null;
 
     // Helper method for setting date in entry Handler
-    private static String set_date() {
+    public static String set_date() {
         Scanner input = new Scanner(System.in);
         String date ="";
         while (input.hasNextLine()) {
@@ -29,7 +29,7 @@ public class DateCalculator {
     }
 
     // Check if the input date is valid or not.
-    private static boolean isValid(String date) {
+    public static boolean isValid(String date) {
         String valid_pattern = "^(3[01]|[12][0-9]|0[1-9])/(1[0-2]|0[1-9])/[0-9]{4}$";
         Pattern pattern = Pattern.compile(valid_pattern);
         Matcher matcher = pattern.matcher(date);
@@ -41,33 +41,36 @@ public class DateCalculator {
             if (Integer.parseInt(arr[2]) < 1901 || Integer.parseInt(arr[2]) > 2999){
                 System.out.println("Invalid year. The year should be between 1901 and 2999.");
                 return false;
-            } else if(!isValidDate(Integer.parseInt(arr[0]),Integer.parseInt(arr[1]),Integer.parseInt(arr[2]))){
+            } else if(!isValidDate(Integer.parseInt(arr[0]),Integer.parseInt(arr[1]),Integer.parseInt(arr[2]))){ // check for valid date
                 return false;
             }
         }else {
-            System.out.println("Invalid date. Accepted Pattern: DD/MM/YYYY. Please try again.");
+            System.out.println("Invalid date pattern. Accepted Pattern: DD/MM/YYYY. Please try again.");
             return false;
         }
         return true;
     }
 
     // Check for invalid dates.
-    private static boolean isValidDate(int day, int month, int year) {
+    public static boolean isValidDate(int day, int month, int year) {
         try {
             LocalDate.of(year,month,day);
         } catch (DateTimeException e) {
-            System.out.println("The Date is invalid. Try again.");
+            System.out.println("The date does not exist. Try again.");
             return false;
         }
         return true;
     }
 
     // Handle date calculation
-    private static long daysBetween(String start, String end) {
+    public static long daysBetween(String start, String end) {
         int[] start_arr = toIntArr(start);
         int[] end_arr = toIntArr(end);
         LocalDate startDate = LocalDate.of(start_arr[2],start_arr[1],start_arr[0]);
         LocalDate endDate = LocalDate.of(end_arr[2],end_arr[1],end_arr[0]);
+        if (startDate.equals(endDate)){
+            return 0;
+        }
         long daysBetween = ChronoUnit.DAYS.between(startDate,endDate);
         if (daysBetween < 0){
             daysBetween= daysBetween*-1;
@@ -86,8 +89,8 @@ public class DateCalculator {
     }
 
     // Handle user input.
-    private static void greetUser() {
-        System.out.println("Welcome to Date Calculator. Please choose a data entry method:");
+    public static void greetUser() {
+        System.out.println("Please choose a data entry method:");
         System.out.println("1 : Manual Entry\n2 : Quit Programme\n");
 
         // Validate Input method.
@@ -96,20 +99,10 @@ public class DateCalculator {
             if (command.isEmpty()) {
                 System.out.println("Please enter a number.");
             }
-
             // Input handler
             if (command.matches("[1-2]")) {
-                if (command.equals("1")) {
-                    System.out.println("Please enter start date. Date format accepted :  DD/MM/YYYY.");
-                    startDate = set_date();
-                    // set end Date and validate
-                    System.out.println("Please enter end date. Date format accepted :  DD/MM/YYYY.");
-                    endDate = set_date();
-                    break;
-                } else if (command.equals("2")) {
-                    System.out.println("Now quiting ...\nThank you for using date calculator.");
-                    System.exit(0);
-                }
+                entry_handler(command);
+                break;
             } else {
                 System.out.println("Invalid input.Try again.");
                 continue;
@@ -117,49 +110,59 @@ public class DateCalculator {
         }
     }
 
+    public static void entry_handler(String command) {
+        if (command.equals("1")) {
+            System.out.println("Please enter start date. Date format accepted :  DD/MM/YYYY.");
+            startDate = set_date();
+            // set end Date and validate
+            System.out.println("Please enter end date. Date format accepted :  DD/MM/YYYY.");
+            endDate = set_date();
+        } else if (command.equals("2")) {
+            System.out.println("Thank you for using date calculator.");
+            System.exit(0);
+        }
+    }
+
     // Validate command line arguments
-    private static void argument(String[] args) {
+    public static void argument(String[] args) {
         if (args.length != 0) {
             // validate argument len
             if (args.length > 2 ) {
-                System.out.println("\nToo many arguments.\nQuiting date calculator ...");
-                System.exit(1);
+//                System.out.println ();
+                throw new IllegalArgumentException("Too many arguments.");
             } else if (args.length < 2){
-                System.out.println("\nMissing arguments. Please enter an extra date and try again.\nQuiting date calculator.");
-                System.exit(1);
+//                System.out.println("\nMissing arguments. Please enter an extra date and try again.\nQuiting date calculator.");
+                throw new IllegalArgumentException("Missing arguments.");
             } else {
                 // validate input
                 if (isValid(args[0])){
                     startDate = args[0];
                 } else {
-                    System.out.println("\nERROR : Invalid start date\nQuiting date calculator. Please try again.");
-                    System.exit(1);
+                    throw new IllegalArgumentException("ERROR : Invalid start date");
                 }
+
                 if (isValid(args[1])){
                     endDate = args[1];
-                    System.out.println("Command line argument detected ! Here is the result!\n\n");
                 }else {
-                    System.out.println("\nERROR : Invalid end date.\nQuiting date calculator. Please try again.");
-                    System.exit(1);
+                    throw new IllegalArgumentException("ERROR : Invalid end date.");
                 }
             }
         }
     }
 
     // Calculate days between
-    private static void result(String startDate, String endDate){
-        System.out.println("\nCalculating days between ...");
+    public static void result(String startDate, String endDate){
         long result = daysBetween(startDate,endDate);
-        System.out.println("Days Between : " + result + " days\n");
-        System.out.println("Thank you for using date calculator. See you next time.");
+        System.out.println(result + " days\n");
     }
 
     public static void main(String[] args) {
-        // Check if there is command line argument. If not th
-        DateCalculator.argument(args); // TODO : Test case for main.argument method
+        System.out.println("Welcome to Date Calculator.\n");
         // If there is no command line argument, choose option.
         if (args.length == 0){
             DateCalculator.greetUser(); // TODO : Test case for main argument method.
+        } else {
+            DateCalculator.argument(args); // TODO : Test case for main.argument method
         }
         // Calculate the days between two dates
         DateCalculator.result(startDate,endDate); // TODO : test case for calculate days between
---
+++ src/com/company/DateCalculatorTest.java
@@ -0,0 +1,170 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateCalculatorTest {
+    // Tests for Command Line Argument Handler and validation
+    @Test
+    void CheckExtraArguments(){
+        String[] input ={"01/01/1996","01/01/1989","01/01/2000"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("Too many arguments.",exception.getMessage());
+    }
+
+    @Test
+    void CheckMissingArguments() {
+        String[] input = {"01/01/1996", "01/01/1989", "01/01/2000"};
+        Exception exception = assertThrows(Exception.class, () -> DateCalculator.argument(input));
+        assertEquals("Too many arguments.", exception.getMessage());
+    }
+
+    // Impossible Dates Validation Check
+    @Test
+    void CheckArgumentsImpossibleStartMonth(){
+        String[] input ={"01/00/1996","01/01/1996"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid start date",exception.getMessage());
+    }
+
+    @Test
+    void CheckArgumentsImpossibleStartDay(){
+        String[] input ={"32/01/1996","01/01/1996"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid start date",exception.getMessage());
+    }
+
+    // Start Date Validation Check
+    @Test
+    void CheckArgumentsStartYearBefore(){
+        String[] input ={"31/01/1899","01/01/1996"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid start date",exception.getMessage());
+
+    }
+
+    @Test
+    void CheckArgumentsStartDatesAfter(){
+        String[] input ={"31/01/3000","01/01/1996"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid start date",exception.getMessage());
+    }
+
+    // End Date Validation Check
+    @Test
+    void CheckArgumentsEndDatesBefore(){
+        String[] input ={"31/01/1901","01/01/1899"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid end date.",exception.getMessage());
+    }
+
+    @Test
+    void CheckArgumentsEndDatesAfter(){
+        String[] input ={"31/01/1901","01/01/3000"};
+        Exception exception = assertThrows(Exception.class,()->DateCalculator.argument(input));
+        assertEquals("ERROR : Invalid end date.",exception.getMessage());
+    }
+
+    // Test for validity check for dates
+    @Test
+    void CheckPattern1() {
+        assertEquals(false, DateCalculator.isValid("31/0/2019"));
+    }
+
+    @Test
+    void CheckPattern2() {
+        assertEquals(false, DateCalculator.isValid("3/01/2019"));
+    }
+
+    @Test
+    void CheckPattern3() {
+        assertEquals(false, DateCalculator.isValid("31/01/19"));
+    }
+
+    @Test
+    void CheckPattern4() {
+        assertEquals(true, DateCalculator.isValid("31/01/2019"));
+    }
+
+    @Test
+    void CheckPattern5() {
+        assertEquals(true, DateCalculator.isValid("31/03/2019"));
+    }
+
+    @Test
+    void CheckPattern6() {
+        assertEquals(false, DateCalculator.isValid("1/0/1"));
+    }
+
+    @Test
+    void Check_Impossible_Days() {
+        assertEquals(false, DateCalculator.isValid("31/04/2019"));
+    }
+
+    @Test
+    void CheckInvalidMonth1() {
+        assertEquals(false, DateCalculator.isValid("31/13/2019"));
+    }
+
+    @Test
+    void CheckInvalidMonth2() {
+        assertEquals(false, DateCalculator.isValid("31/00/2019"));
+    }
+
+    @Test
+    void CheckIsValidDateLower() {
+        assertEquals(false,DateCalculator.isValid("01/01/1900"));
+    }
+
+    @Test
+    void CheckIsValidDateUpper() {
+        assertEquals(false,DateCalculator.isValid("01/01/3000"));
+    }
+
+    // Test for days between calculation and logic
+    @Test
+    void CheckLogic1() {
+        assertEquals(1,DateCalculator.daysBetween("01/01/2000","03/01/2000"));
+    }
+
+    @Test
+    void CheckLogic2() {
+        assertEquals(1,DateCalculator.daysBetween("03/08/1983","05/08/1983"));
+    }
+
+    @Test
+    void CheckSameDay() {
+        assertEquals(0,DateCalculator.daysBetween("01/01/2000","01/01/2000"));
+    }
+
+    @Test
+    void CheckDaysBetween2() {
+        assertEquals(19,DateCalculator.daysBetween("02/06/1983","22/06/1983"));
+    }
+
+    @Test
+    void CheckDaysBetween3() {
+        assertEquals(173,DateCalculator.daysBetween("04/07/1984","25/12/1984"));
+    }
+
+    @Test
+    void CheckDaysBetween4() {
+        assertEquals(1979,DateCalculator.daysBetween("03/01/1989","03/08/1983"));
+    }
+
+    @Test
+    void CheckSwappingPosition() {
+        assertEquals(1979,DateCalculator.daysBetween("03/08/1983","03/01/1989"));
+    }
+
+    @Test
+    void CheckZeroDay() {
+        assertEquals(0,DateCalculator.daysBetween("03/08/1983","03/08/1983"));
+    }
+
+}
